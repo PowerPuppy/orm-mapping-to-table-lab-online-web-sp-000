a27711c13364ec04a44e1e510a4b26b2510cdc55
Chris Cheong <chrisxcheong@gmail.com>
2019-11-26 20:17:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,23 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
+    def self.create_table
+      sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+      DB[:conn].execute(sql)
+    end
 
+  def self.drop_table
+      sql = <<-SQL
+        DROP TABLE IF EXISTS students
+        SQL
+      DB[:conn].execute(sql)
+    end
 
 
 end
